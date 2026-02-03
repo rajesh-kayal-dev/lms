d20033915d449d889de6207c678ab39908318208
rajesh-kayal-dev <rk6289943975@gmail.com>
2026-02-04 00:50:47 +0530

    connection with frontend to bacckend

--- client/src/components/pages/Register.jsx
+++ client/src/components/pages/Register.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
 import Layout from '../common/Layout'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { apiUrl } from '../common/Config'
 
 const Register = () => {
+  const {
+    handleSubmit, register, formState: { errors }, setError
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    // console.log(data);
+    await fetch(`${apiUrl}/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then(res => res.json())
+      .then(data => {
+        console.log(data);
+      })
+  }
+
   return (
     <Layout>
       <div className='container py-5 mt-5'>
         <div className='d-flex align-items-center justify-content-center'>
-          <form >
+          <form onSubmit={handleSubmit(onSubmit)} >
             <div className='card border-0 shadow register'>
               <div className='card-body p-4'>
                 <h3 className='border-bottom pb-3 mb-3'>Register</h3>
@@ -15,26 +36,57 @@ const Register = () => {
                 <div className='mb-3'>
                   <label className='form-label' htmlFor="name">Name</label>
                   <input
+                    {
+                    ...register('name', {
+                      required: 'Name is required',
+                    })
+                    }
                     type="text"
-                    className={`form-control`}
+                    className={`form-control ${errors.name && 'is-invalid'}`}
                     placeholder='Name' />
+
+                  {
+                    errors.name && <span className='invalid-feedback'>{errors.name.message}</span>
+                  }
                 </div>
 
 
                 <div className='mb-3'>
                   <label className='form-label' htmlFor="email">Email</label>
                   <input
-                    type="text" className={`form-control`}
+                    {
+                    ...register('email', {
+                      required: 'Email is required',
+                      pattern: {
+                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                        message: 'Invalid email address'
+                      }
+                    })
+                    }
+                    type="text" className={`form-control ${errors.email && 'is-invalid'}`}
                     placeholder='Email' />
+
+                  {
+                    errors.email && <span className='invalid-feedback'>{errors.email.message}</span>
+                  }
 
                 </div>
 
                 <div className='mb-3'>
                   <label className='form-label' htmlFor="password">Password</label>
                   <input
+                    {
+                    ...register('password', {
+                      required: 'Password is required',
+                    })
+                    }
                     type="password"
-                    className={`form-control`}
+                    className={`form-control ${errors.password && 'is-invalid'}`}
                     placeholder='Password' />
+
+                  {
+                    errors.password && <span className='invalid-feedback'>{errors.password.message}</span>
+                  }
                 </div>
 
                 <div>
@@ -49,7 +101,7 @@ const Register = () => {
           </form>
         </div>
       </div>
-    </Layout> 
+    </Layout>
   )
 }
 
